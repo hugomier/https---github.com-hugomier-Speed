37221992c6f9972d0309ceddfbd39184bff6650a
hugomier <hugomiert14@gmail.com>
2025-09-10 22:21:09 -0500

    11

---
+++ src/test/java/com/speed/app_v1/AppV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speed.app_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
