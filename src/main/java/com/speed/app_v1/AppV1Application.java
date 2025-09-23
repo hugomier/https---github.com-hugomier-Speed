86090e925a4fa63ea6ea6ae8a64714ff1a1aaf21
richardarboleda10 <richard.arboleda@est.iudigital.edu.co>
2025-09-23 13:48:00 -0500

    Se agrega el controller del index/home, se corrige error de dependencia en pom.

--- src/main/java/com/speed/app_v1/AppV1Application.java
+++ src/main/java/com/speed/app_v1/AppV1Application.java
@@ -8,6 +8,7 @@ public class AppV1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppV1Application.class, args);
+		System.out.println("Aplicacion iniciada");
 	}
 
 }
--- src/main/java/com/speed/app_v1/Controller/indexController.java
+++ src/main/java/com/speed/app_v1/Controller/indexController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequestMapping("/")
 public class indexController {
-    @GetMapping("home")
-    public String home () {
+    @GetMapping("/index")
+    public String index () {
         return "index";
     }
     
