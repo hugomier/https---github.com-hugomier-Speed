db3bcacdf847e3956943237eaacef30cf4c21bfb
richardarboleda10 <richard.arboleda@est.iudigital.edu.co>
2025-09-24 16:47:31 -0500

    Se agregan rutas al IndexControler con su respectivo template: mision, vision, valores y servicios. Se agrega carpeta fragments para modularizar el html en un futuro.

--- src/main/java/com/speed/app_v1/Controller/indexController.java
+++ src/main/java/com/speed/app_v1/Controller/indexController.java
@@ -1,8 +1,8 @@
 package com.speed.app_v1.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
@@ -11,6 +11,26 @@ public class indexController {
     @GetMapping("/index")
     public String index () {
         return "index";
+    }
+
+    @GetMapping("/mision")
+    public String mision () {
+        return "mision";
+    }
+    
+    @GetMapping("/vision")
+    public String vision () {
+        return "vision";
+    }
+    
+    @GetMapping("/valores")
+    public String valores () {
+        return "valores";
+    }
+
+    @GetMapping("/servicios")
+    public String servicios () {
+        return "servicios";
     }
     
 }
